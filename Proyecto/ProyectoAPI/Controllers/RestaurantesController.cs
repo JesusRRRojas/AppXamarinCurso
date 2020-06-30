c557842a4c29b661bcee0794655af30fc283f194
JesusRRRojas <47578400+JesusRRRojas@users.noreply.github.com>
2020-06-29 21:13:57 -0500

    Agregando listas

--- Proyecto/ProyectoAPI/Controllers/RestaurantesController.cs
+++ Proyecto/ProyectoAPI/Controllers/RestaurantesController.cs
@@ -3,15 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProyectoAPI.Data;
+using ProyectoAPI.Entities.Models;
+using ProyectoAPI.Services;
 
 namespace ProyectoAPI.Controllers
 {
     [Route("api/v1/XamarinCurso/[Controller]")]
     public class RestaurantesController : Controller
     {
-        public IActionResult Index()
+        private dbaplicationContext _context = new dbaplicationContext();
+        private UnitOfWork _unitOfWork = new UnitOfWork(new dbaplicationContext());
+
+        [HttpGet("idUsuario")]
+        public IActionResult GetAllRestaurant(int idUsuario)
         {
-            return View();
+            var _listarestaurantes = new List<ListaRestaurantes>();
+
+            var _restaurantes = new List<Restaurant>();
+            var _listafavoritousaurio = new List<Favorito>();
+
+            _restaurantes = ObtenerRestaurantes();
+            _listafavoritousaurio = ObtenerFavoritos(idUsuario);
+
+            foreach (var item in _restaurantes)
+            {
+                _listarestaurantes.Add(new ListaRestaurantes()
+                {
+                    IdRestaurant = item.IdRestaurant,
+                    NombreRestaurant = item.Nombre,
+                    DescripcionRestaurant = item.Descripcion,
+                    DireccionRestaurant = item.Direccion,
+                    //Categorias = _unitOfWork.Categorias.Get(x=>x.)
+                });
+            }
+
+            return Ok(_listarestaurantes);
         }
+        private void ObtenerCategorias(int idRestaurant)
+        {
+
+        }
+        private List<Restaurant> ObtenerRestaurantes()
+        {
+            return _unitOfWork.Restaurantes.Get().
+        }
+        private List<Favorito> ObtenerFavoritos(int idusuario)
+        {
+            return _unitOfWork.Favoritos.Get(x => x.IdUsuario == idusuario).ToList();
+
+        }
+        private bool BuscarFavorito(List<Favorito> listaFavoritoUsuario, int idRestaurant)
+        {
+            return true;
+        }
+        // private List<Califi>
+
     }
 }
